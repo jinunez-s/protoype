4cdcb6dfc406ca8639fc357ad70c1978ac2ebc00
Julio Nuñez <julionr14@gmail.com>
2022-03-28 14:05:28 -0600

    Add React

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 const Express = require("express"); // Importar en nodejs
 const RequestHandler = require("./handlers/todos");
 const { initializeDB } = require("./lib/db");
-
 const App = Express();
 
 App.use(Express.json());
 App.use(Express.urlencoded( { extended: false}))
+
 App.use(RequestHandler)
 
 App.listen(3000, () =>{
